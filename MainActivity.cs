0b96d943b1f74e4ff41efb755c1a30dfd9259932
eddydn <eddydn@gmail.com>
2016-11-21 12:45:01 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+
+namespace AndroidMultiLineNotification
+{
+    [Activity(Label = "AndroidMultiLineNotification", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int mNotification = 100;
+
+       
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+             Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate {
+                Intent newIntent = new Intent(this, typeof(MainActivity));
+                PendingIntent resultPendingIntent = PendingIntent.GetActivity(this, 0, newIntent, PendingIntentFlags.CancelCurrent);
+
+                string content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .SetAutoCancel(true)
+                .SetContentIntent(resultPendingIntent)
+                .SetContentTitle("Multiline Notification")
+                .SetSmallIcon(Resource.Drawable.Icon)
+                .SetStyle(new NotificationCompat.BigTextStyle().BigText(content))//Set Multi Line
+                .SetContentText(content);
+
+                NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+                notificationManager.Notify(mNotification, builder.Build());
+
+            };
+        }
+    }
+}
+
